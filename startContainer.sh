912b38bb2af7207e031b4ecd1a7a872728a59de9
root <root@core.machinazero.com>
2023-05-11 00:33:40 +1000

    updated

--- startContainer.sh
+++ startContainer.sh
@@ -1,1 +1,1 @@
-docker run -d --restart unless-stopped viper796/electrobot
+docker run -d --restart unless-stopped -e BOT_NICKSERV_USER='electrobot' -e BOT_NICKSERV_PASS='ddrgh427' viper796/electrobot
